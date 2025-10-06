39905abcc2eb9a858d791000f320a11ca09b1fae
luttinenjoni <luttinenjoni@gmail.com>
2025-10-06 15:26:27 +0300

    Answers added

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,6 @@
-// Haetaan nappi ja kaikki vastaukset
 const button = document.getElementById("showAnswersBtn");
 const answers = document.querySelectorAll(".answer");
 
-// Lisätään toiminto napille
 button.addEventListener("click", function() {
   answers.forEach(answer => {
     answer.style.display = "block";
